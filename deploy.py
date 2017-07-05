25644fcecaa615b8f88261a81d189258390b58c1
Samyak Parajuli <sparajuli@Samyaks-MacBook-Pro.local>
2017-07-04 17:29:04 -0700

    matplotlib

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from content_management import Content 
 # import sys
 # import logging
@@ -27,6 +27,36 @@ def prop_logic():
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html")
+
+@app.route("/simple.png")
+def simple():
+    import datetime
+    import StringIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 # app.logger.addHandler(logging.StreamHandler(sys.stdout))
 # app.logger.setLevel(logging.ERROR)
