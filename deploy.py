6bfcf2f40c364ac45601c67853793df52c3e9d76
Samyak Parajuli <sparajuli@Samyaks-MacBook-Pro.local>
2017-07-11 01:20:22 -0700

    finally adding content lol10

--- deploy.py
+++ deploy.py
@@ -32,7 +32,8 @@ def prop_logic():
 @app.route("/chapter1/")
 def basic_nn():
 	return render_template("chapter1nn.html")
-@app.route("/2010_AIME_1_#6")
+@app.route("/2012_AIME_1_#6")
+def AIME_2012():
 	return render_template("2012_AIME_1_6.HTML")
 
 @app.errorhandler(404)
