14076a3da30014e0b5b9f58b63abc25bc8913d16
Samyak Parajuli <sparajuli@Samyaks-MacBook-Pro.local>
2017-08-12 14:18:34 -0700

    deeplearning

--- content_management.py
+++ content_management.py
@@ -1,7 +1,7 @@
 def Content():
 	TOPIC_DICT = {"Basics":[["Proposition", "/lecture1/"], ["Proofs", "/chapter2/"], ["Induction", "/chapter3/"], ["Stable Marriage", "/lecture4/"],
 	["Graph Theory", "/lecture5/"]], 
-	"Neural_Nets":[["Chapter1", "/chapter1/"], ["Chapter2", "/chapter2/"], ["Convnets", "/convnets/"]],
+	"Neural_Nets":[["Chapter1", "/chapter1/"], ["Chapter2", "/chapter2/"], ["Convnets", "/convnets/"], ["DeepLearning", "/deeplearning/"]],
 	"Random_Math":[["2012 AIME I #6", "/2012_AIME_6/"], ["Putnam A2 2000", "/A2_Putnam_2000/"], ["Taylor+Fourier", "/Taylor+Fourier/"]]}
 
 	return TOPIC_DICT
